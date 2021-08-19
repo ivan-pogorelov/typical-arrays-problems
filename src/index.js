ee83b5351f558a9c2baa81487f04e07c5eb67839
ivan-pogorelov <ivanpogorelov97a@gmail.com>
2021-08-19 22:40:38 +0300

    finished task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (array === undefined) {
+    return 0;
+  }
+
+  return array.sort(function(a, b) {
+		return b - a;
+	}).slice(array.length - 1).join();
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (array === undefined) {
+    return 0;
+  }
+
+  return array.sort(function(a, b) {
+		return a - b;
+	}).slice(array.length - 1).join();
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  let numAvg = array;
+  let result = 0;
+
+  if (array === undefined) {
+    return 0;
+  }
+
+	for (i = 0; i < numAvg.length; i++) {
+		result += numAvg[i];
+    
+	};
+
+  if (result === 0) {
+    return 0;
+  }
+
+	return result / numAvg.length;
+
 }
